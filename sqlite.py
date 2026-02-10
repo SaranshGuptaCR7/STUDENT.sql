f3e4afb7bb62ee7ab0d0780b66f0e7daff96fcaa
SaranshGuptaCR7 <saransh14.gupta@gmail.com>
2026-02-10 19:34:45 +0530

    python
    pythonw23

---
+++ sqlite.py
@@ -0,0 +1,8 @@
+print("Guess which database is this-")
+print("1. Rational")
+print("2. Non rational")
+answer = int(input("Enter your answer: "))
+if answer == 1:
+    print("You got it rightttttt!!")
+else:
+    print("Wrong wrong very wrong answer!!")
