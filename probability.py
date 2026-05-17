e94dcaef816a311b919e6a3686a5ab31a402ebd7
SaranshGuptaCR7 <saransh14.gupta@gmail.com>
2026-05-17 17:15:15 +0530

    probability after class

--- probability.py
+++ probability.py
@@ -1,8 +1,9 @@
-import random
-import matplotlib.pyplot as pd
-import seaborn as sns
-balls = ['red', 'yellow', 'blue']
-pro = random.choice(balls)
-result = balls.count('red')/len(balls)
-if pro == 'red':
-    print("Red ball is selected")
+def prob(totaal, a):
+    prob_a = a / totaal
+    totaal = 6
+    return prob_a
+    if prob_a == 6:
+        print("The game has started")
+    else:
+        print("The game has not started") 
+print("The probability of getting six is:", prob(6, 1))        
