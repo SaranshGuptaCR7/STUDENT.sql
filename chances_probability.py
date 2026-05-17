ae643626f393d2cf60123dd82f59a00bbebad9c9
SaranshGuptaCR7 <saransh14.gupta@gmail.com>
2026-05-17 17:08:16 +0530

    probability chances

---
+++ chances_probability.py
@@ -0,0 +1,9 @@
+rb = 1
+bb = 6
+wb = 3
+total = rb + bb + wb
+prob_wb = wb / total
+print("The probability of drawing a white ball is:", prob_wb)
+w_prob = prob_wb
+probability = w_prob * prob_wb
+print("The probability of drawing two white balls given that first and second balls are white:", probability)
